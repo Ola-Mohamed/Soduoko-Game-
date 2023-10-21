4d1d98438c9419544a5c92c964524d6c2e00eace
Ola-Mohamed <Ola180483@feng.bu.edu.eg>
2023-10-21 23:05:18 +0300

    final

---
+++ Src/Header/Utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_H_
+
+
+#define    BAR_SIZE             100
+#define    BAR_SPEED            20
+
+
+void musicPlay();
+void musicStop();
+void loadingBar(int milliseconds, int totalBars) ;
+void clear_console();
+
+
+
+
+
+#define UTILS_H_
+#endif // UTILS_H_
